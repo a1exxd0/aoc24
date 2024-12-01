8a7cd732eb48ba57a6ce7401b4b81cb09bf9625c
Alex Do <alexxdo2004@gmail.com>
2024-12-01 14:05:45 +0000

    style: tidy code

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -3,46 +3,51 @@ advent_of_code::solution!(1);
 use std::collections::HashMap;
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let (mut vec1, mut vec2): (Vec<i32>, Vec<i32>) = input
+    let (mut vec1, mut vec2): (Vec<u32>, Vec<u32>) = input
         .lines()
-        .map(|line| {
+        .filter_map(|line| {
             let mut parts = line.split_whitespace();
-            (
-                parts.next().unwrap().parse::<i32>().unwrap(),
-                parts.next().unwrap().parse::<i32>().unwrap(),
-            )
+            Some((
+                parts.next()?.parse().ok()?,
+                parts.next()?.parse().ok()?,
+            ))
         })
-        .unzip();
+        .fold(
+            (Vec::new(), Vec::new()),
+            |(mut vec1, mut vec2), (a, b)| {
+                vec1.push(a);
+                vec2.push(b);
+                (vec1, vec2)
+            }
+        );
 
     vec1.sort(); vec2.sort();
 
-    vec1.iter()
-        .zip(vec2.iter())
-        .map(|(a, b)| (a - b).abs() as u32)
-        .sum::<u32>()
-        .into()
+    Some (
+        vec1.iter().zip(vec2.iter()).map(|(a, b)| (a - b)).sum()
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let (vec1, vec2): (Vec<u32>, Vec<u32>) = input
+    let (vec, occurences): (Vec<u32>, HashMap<u32, u32>) = input
         .lines()
-        .map(|line| {
+        .filter_map(|line| {
             let mut parts = line.split_whitespace();
-            (
-                parts.next().unwrap().parse::<u32>().unwrap(),
-                parts.next().unwrap().parse::<u32>().unwrap(),
-            )
+            Some((
+                parts.next()?.parse().ok()?,
+                parts.next()?.parse().ok()?,
+            ))
         })
-        .unzip();
+        .fold(
+            (Vec::new(), HashMap::new()),
+            |(mut vec, mut occurences), (a, b)| {
+                vec.push(a);
+                *occurences.entry(b).or_default() += 1;
+                (vec, occurences)
+            },
+        );
 
-    let occurences: HashMap<u32, u32> = vec2
-        .iter()
-        .fold(HashMap::new(), |mut acc, &n| {
-            *acc.entry(n).or_default() += 1;
-            acc
-        });
-
-    let result = vec1
+    let result = vec
         .iter()
         .map(|&n| n * occurences.get(&n).copied().unwrap_or(0))
         .sum();
